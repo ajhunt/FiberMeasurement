7be3e378d2ed1ee092d105fcee450692b528c685
ajhunt <ajhunt@CAREY-GRAD21.engineering.ualberta.ca>
2017-02-02 15:30:42 -0700

    added max circumferential angle as function input

--- surfaceunwrapfunction.m
+++ surfaceunwrapfunction.m
@@ -1,9 +1,11 @@
-function [ unwrapIm] = surfaceunwrapfunction( im, leftBoundary, rightBoundary, centerLine )
+function [ unwrapIm] = surfaceunwrapfunction( im, leftBoundary, rightBoundary, centerLine, maxAngle )
 %applied orthographic projection to images of the tubular braid surface
 
 %inputs: im = input image, leftBoundary, rightBoundary, centerLine =
 %feature locations in image, braidDiameter = mandrel diameter of braid in
 %either mm or inches, unit = 0 for imperial, 1 for metric
+%maxAngle = maximum circumferential angle that is unwrapped - if this value
+%is too large (>65 degrees) resulting image is becomes distorted.  
 
 braidDiameterPX = round(rightBoundary) - round(leftBoundary);  %braid diameter in pixels
 braidRadiusPX = round(braidDiameterPX/2);
@@ -18,24 +20,30 @@ padSize = 1000;
 imMap = uint8(zeros(sizeY,sizeX+padSize));
 
 %Pixel re-mapping
-for i = 1:round(braidDiameterPX/2)
-    arclength(i) = asin(i/braidRadiusPX)*braidRadiusPX; %arclength = theta*r
+for i = 1:braidRadiusPX
+    angle(i) = rad2deg(asin(i/braidRadiusPX));
+    arclength(i) = deg2rad(angle(i))*braidRadiusPX; %arclength = theta*r
+    if angle(i) < maxAngle
+        maxInd = i;
+    else
+        continue
+    end
 end
 
 %Define the amount of unwrapping as a function of distance from centerline
-x = 1:braidRadiusPX;
-unWrap = real(round(arclength-x));
+x = 1:maxInd;
+unWrap = real(round(arclength(1:maxInd)-x));
 
 %looping through the columns on the right side of the braid centerline
 
 count = 1;
-for i = centerLine+1:1:rightBoundary
+for i = centerLine+1:1:centerLine+maxInd %right side
     imMap(:,i+unWrap(count)+padSize) = im(:,i);
     count = count+1;
 end
 
 count = 1;
-for j = centerLine-1:-1:leftBoundary
+for j = centerLine-1:-1:centerLine-maxInd
     imMap(:,j-unWrap(count)+padSize) = im(:,j);
     count = count+1;
 end
@@ -99,7 +107,7 @@ Vq = F(queryPoints);
 %replace regions of missing data in image with interpolated values
 unwrapIm = imMapResize;
 for i = 1:length(Vq)
-   inwrapIm(Iq(i), Jq(i)) = Vq(i); 
+   unwrapIm(Iq(i), Jq(i)) = Vq(i); 
 end
 
 end
